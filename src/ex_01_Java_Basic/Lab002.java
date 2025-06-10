91b19c1e173389032d5fd90c83e544efe6705668
prashant90-dotcom <prashantpawar1019@gmail.com>
2025-06-10 21:09:42 +0530

    10 june task done

---
+++ src/ex_01_Java_Basic/Lab002.java
@@ -0,0 +1,21 @@
+package ex_01_Java_Basic;
+
+public class Lab002 {
+
+    public static void main (String [] args){
+
+         //System.out.printf("%d * %d= %d",a,b,a);
+        System.out.printf("9 * 1 = %d%n", 9 * 1);
+        System.out.printf("9 * 2 = %d%n", 9 * 2);
+        System.out.printf("9 * 3 = %d%n", 9 * 3);
+        System.out.printf("9 * 4 = %d%n", 9 * 4);
+        System.out.printf("9 * 5 = %d%n", 9 * 5);
+        System.out.printf("9 * 6 = %d%n", 9 * 6);
+        System.out.printf("9 * 7 = %d%n", 9 * 7);
+        System.out.printf("9 * 8 = %d%n", 9 * 8);
+        System.out.printf("9 * 9 = %d%n", 9 * 9);
+        System.out.printf("9 * 10 = %d%n", 9 * 10);
+    }
+
+
+}
